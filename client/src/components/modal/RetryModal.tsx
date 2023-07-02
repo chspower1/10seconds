775e0e3e3b0ac438fe455ee2738d2eac7cfae2bd
chs <chspower1@gmail.com>
2023-07-02 22:50:56 +0900

    feat : RetryModal 컴포넌트 생성

--- client/src/components/modal/RetryModal.tsx
+++ client/src/components/modal/RetryModal.tsx
@@ -1,4 +1,33 @@
+import { useSelector } from "react-redux";
+import { closeRetry, selectmodal } from "../../store/slice/modalSlice";
+import Overlay from "../layout/Overlay";
+import { useAppDispatch, useAppSelector } from "../../store/hooks";
+import ModalContainer from "./ModalContainer";
+import { startAndStopCount } from "../../store/slice/countSlice";
+
 const RetryModal = () => {
-  return <>retry?</>;
+  const { openRetryModal } = useSelector(selectmodal);
+  const dispatch = useAppDispatch();
+
+  const handleClickRetry = () => {
+    dispatch(closeRetry());
+    dispatch(startAndStopCount());
+  };
+  return (
+    <>
+      {openRetryModal && (
+        <>
+          <ModalContainer>
+            <h3>재도전 하시겠습니까?</h3>
+            <div className="flex gap-3 mt-10">
+              <button onClick={handleClickRetry}>Yes</button>
+              <button onClick={() => dispatch(closeRetry())}>No</button>
+            </div>
+          </ModalContainer>
+          <Overlay onClick={() => dispatch(closeRetry())} />
+        </>
+      )}
+    </>
+  );
 };
 export default RetryModal;
